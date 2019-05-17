d5d3a7fa95f73536ae8397017882eb5858dea276
Hilal Jaffan <hj@jaffan.io>
2019-05-17 05:16:40 -0700

    Adding variable descriptions

--- modules/managed_sql/variables.tf
+++ modules/managed_sql/variables.tf
@@ -15,47 +15,58 @@ variable "databaseResourceName" {
 // Managed Insance Variables
 variable "managedInstanceName" {
   type = "string"
+  description = "Enter managed instance name."
 }
 
 variable "administratorLogin" {
     type = "string"
+    description = "Enter user name."
 }
 
 variable "administratorLoginPassword" {
     type = "string"
-  
+    description = "Enter password."
 }
 variable "location" {
   type = "string"
+  description = "Enter location. If you leave this field blank resource group location would be used."
 }
 
 variable "virtualNetworkName" {
     type = "string"
+    description = "Enter virtual network name. If you leave this field blank name will be created by the template."    
 }
 
 variable "addressPrefix" {
   type = "string"
+  description = "Enter virtual network address prefix."
 }
 
 variable "subnetName" {
   type = "string"
+  description = "Enter subnet name."
 }
 
 variable "subnetPrefix" {
   type = "string"
+  description = "Enter subnet address prefix."
 }
 
 variable "skuName" {
   type = "string"
+  description = "Enter sku name."
 }
 
 variable "vCores" {
     type = "int"
+    description = "Enter number of vCores."
 }
 variable "storageSizeInGB" {
-  type = "string"
+  type = "int"
+  description = "Enter storage size."
 }
 
 variable "licenseType" {
   type = "string"
+  description = "Enter license type."
 }
