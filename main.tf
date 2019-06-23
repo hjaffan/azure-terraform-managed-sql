7813d6b47de1d861ec9506c9d668bd0f2e27d2ef
Hilal Jaffan <hj@jaffan.io>
2019-06-23 10:27:07 -0700

    Updating module parameters

--- main.tf
+++ main.tf
@@ -2,13 +2,16 @@ resource "azurerm_resource_group" "default" {
   name     = "myTestRG"
   location = "West US"
 }
+
 module "manage_mssql" {
   source = "./modules/managed_sql"
   managedInstanceName = ""
   administratorLogin = ""
   administratorLoginPassword = ""
-  azurerm_resource_group_name = "${azurerm_resource_group.default.name}"
-  location = "${azurerm_resource_group.default.location}"
+  azurermResourceGroupName = ""
+  location = "westus"
+  networkName = ""
+  subnetName = ""
   licenseType = "BasePrice"
   skuName = "GP_Gen5"
 }
